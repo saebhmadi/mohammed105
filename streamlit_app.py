b17d6350190f4d6094879a2b5eda0ce75f7ae0c7
saebhmadi <dddrrraa123aa@gmail.com>
2026-01-31 17:53:35 +0200

    Update streamlit_app.py
    Signed-off-by: saebhmadi <dddrrraa123aa@gmail.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
-streamlit_app.py
 import streamlit as st
 
+st.set_page_config(page_title="موقعي", layout="wide")
+
 st.title("🚀 الموقع يعمل بنجاح")
-st.write("إذا ظهرت هذه الصفحة فكل شيء تمام ✅")
+st.write("إذا ظهرت هذه الصفحة، فكل شيء تمام ✅")
